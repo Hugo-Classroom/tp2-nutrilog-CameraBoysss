a41bab23067053d25e4f27955bd325d04251e819
CameraBoysss <monrenry@gmail.com>
2025-11-13 11:44:32 -0500

    feat: implementing pages

--- NutriLog/Screens/DailySummaryView.swift
+++ NutriLog/Screens/DailySummaryView.swift
@@ -11,23 +11,19 @@ struct Calories: Identifiable {
 struct DailySummaryView: View {
     
     private let maxCalories: Int = 2500
-    @State private var usedCalories: Int = 1250
+    @State private var caloriesLeft: Int = 1250
+    @State private var colorieConsumed: Int = 70
+    
+    
+    
+    @State private var proteinProgress = 0.4
+    @State private var carbsProgress = 0.4
+    @State private var fatProgress = 0.4
     
     var body: some View {
-        
-        
-        
         NavigationView {
             VStack {
-                HStack{
-                    Button("", systemImage: "plus") {
-                        
-                    }
-                    .accentColor(Color(.orange))
-                    .offset(x:175, y :-400)
-                    .padding(10)
-                    .navigationTitle("Aujourd'hui")
-                }
+                
                 List() {
                     Section(header: Text("Calories")) {
                         
@@ -37,20 +33,58 @@ struct DailySummaryView: View {
                                     .fontWeight(.bold)
                                     .padding(4)
                                 HStack {
-                                    Text("\(usedCalories)")
+                                    Text("\(caloriesLeft)")
                                         .fontWeight(.bold)
                                     Text("cal")
                                         .foregroundStyle(Color(.gray))                                   
                                 }
                             }
-                            PercentageCircle(progress: Double(usedCalories) / Double(maxCalories), lineWidth: 8, color: Color.green)
+                            PercentageCircle(progress: Double(caloriesLeft) / Double(maxCalories), lineWidth: 8, color: Color.green)
+                            
+                            VStack {
+                                Text("Consommées")
+                                    .fontWeight(.bold)
+                                    .padding(4)
+                                HStack {
+                                    Text("\(colorieConsumed)")
+                                        .fontWeight(.bold)
+                                    Text("cal")
+                                        .foregroundStyle(Color(.gray))
+                                }
+                            }
                         }
                     }
-                    Section(header: Text("Calories")) {
-                        Text("dwanuda")
+                    Section(header: Text("Macros")) {
+                        HStack {
+                            macros(progress: 0.4, selectedColor: .red, macroType: "Proteines", macroTypeLetter: "P", amount: 127, maxAmount: 150)
+                            Spacer()
+                            macros(progress: 0.4, selectedColor: .purple, macroType: "Glucides", macroTypeLetter: "G", amount: 105, maxAmount: 125)
+                            Spacer()
+                            macros(progress: 0.4, selectedColor: .blue, macroType: "Lipides", macroTypeLetter: "L", amount: 35, maxAmount: 100)
+                            
+                        }
+                    }
+                    Section(header: Text("Déjeuner")) {
+                        NavigationLink(destination: FoodDetailView()) {
+                            
+                        }
+                    }
+                    Section(header: Text("Dîner")) {
+                        Text("")
+                        
+                    }
+                    Section(header: Text("Souper")) {
+                        Text("")
                     }
                 }
             }
+            .toolbar {
+                Button("", systemImage: "plus") {
+                    
+                }
+                .accentColor(Color(.orange))
+            }
+            .navigationTitle("Aujourd'hui")
         }
     }
 }
@@ -80,6 +114,66 @@ struct PercentageCircle: View {
         }
         .frame(width: 40, height: 40)
     }
-    
-    
 }
+
+struct PercentageBar: View {
+    var progress: Double
+    var height: CGFloat = 7
+    var color: Color = .blue
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                
+                RoundedRectangle(cornerRadius: height / 2)
+                    .fill(color.opacity(0.1))
+                    .frame(height: height)
+                
+                RoundedRectangle(cornerRadius: height / 2)
+                    .fill(color)
+                    .frame(width: geometry.size.width * progress,
+                           height: height)
+                    .animation(.easeOut(duration: 0.5), value: progress)
+            }
+        }
+        .frame(height: height)
+    }
+}
+
+struct macros: View{
+    var progress: Double
+    var selectedColor: Color
+    var macroType: String
+    var macroTypeLetter: String
+    var amount: Int
+    var maxAmount: Int
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("\(macroTypeLetter)")
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(.white)
+                    .background(selectedColor)
+                    .cornerRadius(20)
+                    .bold()
+                    .font(.system(size: 14))
+                
+                Text("\(macroType)")
+                    .foregroundStyle(selectedColor)
+                    .bold()
+                    .font(.system(size: 12))
+            }
+            PercentageBar(progress: progress, color: selectedColor)
+                .frame(width: 100)
+            HStack {
+                Text("" + String(amount))
+                    .fontWeight(.bold)
+                Text("/ " + String(maxAmount) + "g")
+                    .foregroundStyle(Color(.gray))
+            }
+        }
+        Spacer()
+    }
+}
+
--- NutriLog/Screens/FoodDetailView.swift
+++ NutriLog/Screens/FoodDetailView.swift
@@ -1,11 +1,84 @@
 import SwiftUI
 
 struct FoodDetailView: View {
+    @State private var calAmount: Int = 0
+    @State private var proteinAmount: Int = 0
+    @State private var carbAmount: Int = 0
+    @State private var fatAmount: Int = 0
+    
     var body: some View {
-        Text("Vue Détail d'un aliment")
+        
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(String(calAmount) + " cal")
+                        .foregroundStyle(.secondary)
+                        .font(.title)
+                    Spacer()
+                    
+                    VStack {
+                        Text("\(proteinAmount) g")
+                            .fontWeight(.bold)
+                            .font(.system(size: 20))
+                        Text("Protéines")
+                            .foregroundStyle(.secondary)
+                            .font(.system(size: 14))
+                    }
+                    .padding(4)
+                    VStack {
+                        Text("\(proteinAmount) g")
+                            .fontWeight(.bold)
+                            .font(.system(size: 20))
+                        Text("Glucides")
+                            .foregroundStyle(.secondary)
+                            .font(.system(size: 14))
+                    }
+                    .padding(4)
+                    VStack {
+                        Text("\(proteinAmount) g")
+                            .fontWeight(.bold)
+                            .font(.system(size: 20))
+                        Text("Lipides")
+                            .foregroundStyle(.secondary)
+                            .font(.system(size: 14))
+                    }
+                    .padding(4)
+                }
+                
+                Text("Historique des consommations")
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
+                foodHistory(repas: "Souper", date: "13 octobre 2025", quantite: 100)
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Banane")
+        }
     }
 }
 
 #Preview {
     FoodDetailView()
+}
+struct foodHistory: View {
+    var repas: String
+    var date: String
+    var quantite: Int
+    
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading){
+                Text("\(repas)")
+                    .fontWeight(.bold)
+                Text("\(date)")
+                    .foregroundStyle(.secondary)
+            }
+            Spacer()
+            Text("\(quantite)g")
+                .foregroundStyle(.secondary)
+        }
+        .padding([.top, .bottom], 8)
+        
+    }
 }
